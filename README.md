fd4c2a342f183b202f8370c1e7d6c18857495c8e
Isaac Hayab <isaachayab@gmail.com>
2025-01-02 11:12:59 +0100

    Added ReadME and editted main.rs

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-...
+
+# Project : Hello world in Rust language
+
+Basic Hello world program in the Rust Programming Language.
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,5 @@
-use std::io;
+
 
 fn main() {
-    let mut guess = String::new();
-
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
-    
-    let guess: u32 = guess.trim().parse().expect("Please type a number");
-
-    println!("You guessed: {}", guess);
-    
+    println!("Hello world");
 }
